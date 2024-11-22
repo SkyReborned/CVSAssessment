7f5483cb2df6fbcf25232b03fe80c967ba7dd381
Joshua Gallegos <josh.gallegos@entercom.com>
2024-11-22 02:50:34 -0600

    Initial Commit

---
+++ CVSAssessment/CVSAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVSAssessmentApp.swift
+//  CVSAssessment
+//
+//  Created by SkyReborned on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CVSAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CVSAssessmentTests/CVSAssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  CVSAssessmentTests.swift
+//  CVSAssessmentTests
+//
+//  Created by SkyReborned on 11/22/24.
+//
+
+import Testing
+@testable import CVSAssessment
+
+struct CVSAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
